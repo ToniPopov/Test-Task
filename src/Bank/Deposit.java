9a6cc242960fbca4bf6eaf036f6488739a168f1d
ToniPopov <tonitheonea@gmail.com>
2017-08-27 23:32:02 +0300

    Bank

---
+++ src/Bank/Deposit.java
@@ -0,0 +1,17 @@
+package Bank;
+
+public class Deposit extends BankAcount {
+
+	double mounthSumDepPayment;
+
+	Deposit(String name, int periodMounths, double interestYearProcent, double presentsLeva,
+			double mounthSumDepPayment) {
+		super(name, periodMounths, interestYearProcent, presentsLeva);
+		this.mounthSumDepPayment = mounthSumDepPayment;
+	}
+	public String toString(){
+		return "Name "+this.name+"\nPeriod for Mounth "+this.periodMounths
+				+"\nInterest for an year in % "+this.interestYearProcent+"\nPresents lv"
+				+"\nMounth deposit payent "+this.mounthSumDepPayment;
+	}
+}
